706ba2eca461cae9e8f5fcaacd3ffb8bea1633d5
Ismail <forwork.ismail@gmail.com>
2022-04-05 09:49:14 +0430

    Refactor 13. Roman to Integer

--- 11-20/13.Roman-To-Integer(Easy).js
+++ 11-20/13.Roman-To-Integer(Easy).js
@@ -1,16 +1,26 @@
-const romanToInt = function (s) {
-  let result = 0;
-  let currentNum = 0;
-  const romanNum = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
-
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (currentNum > romanNum[s[i]]) {
-      result -= romanNum[s[i]];
-    } else {
-      result += romanNum[s[i]];
-    }
-    currentNum = romanNum[s[i]];
+var romanToInt = function(s) {
+  let map = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
   }
-
+  
+  let prev = 0;
+  let result = 0;
+  
+  for (let i = s.length - 1; i >= 0; i--) {
+      let value = map[s[i]];
+      if (prev > value) {
+          result -= value;
+      } else {
+          result += value;
+      }
+      prev = value;
+  }
+  
   return result;
 };
